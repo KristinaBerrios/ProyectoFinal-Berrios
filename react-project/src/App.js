67bfab1f0c4fc6f7ab02c988e7ed672754b0cf73
kristinaberrios <krstbest@gmail.com>
2023-08-09 21:40:40 -0300

    Reactjs Project

---
+++ react-project/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Puedes eliminar esta línea si no la necesitas
+import NavBar from './components/NavBar'; // Importa el componente NavBar
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar /> {/* Agrega el componente NavBar */}
+      {/* Resto del contenido de tu aplicación */}
+    </div>
+  );
+}
+
+export default App;
+
+
